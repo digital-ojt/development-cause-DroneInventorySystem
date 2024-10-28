0fc0002fee418d3ec323500b03db1c62247c4001
unknown <z2240235@intra.ctc-g.co.jp>
2024-10-28 17:18:08 +0900

    data1 finish

--- DroneInventorySystem/src/main/java/com/digitalojt/web/entity/CategoryInfo.java
+++ DroneInventorySystem/src/main/java/com/digitalojt/web/entity/CategoryInfo.java
@@ -1,9 +1,13 @@
 package com.digitalojt.web.entity;
 
-import java.sql.Timestamp;
+import java.time.LocalDateTime;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
@@ -19,34 +23,27 @@ import lombok.Setter;
 @Setter
 @Entity
 
+//追記
+@Table(name = "category_info")
+
 public class CategoryInfo 
 {
-
-	/**
-	 * 分類ID
-	 */
 	@Id
-	private int categoryId;
-	
-	/**
-	 * 名前
-	 */
-	private String categoryName;
-	
-	/**
-	 * 削除フラグ
-	 */
-	private String deleteFlag;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "category_id")
+    private Long categoryId;
 
-	/**
-	 * 作成日付
-	 */
-	private Timestamp createDate;
-	
-	/**
-	 * 更新日付
-	 */
-	private Timestamp updateDate;
-	
+    @Column(name = "category_name")
+    private String categoryName;
+
+    @Column(name = "delete_flag")
+    private Integer deleteFlag;
+
+    @Column(name = "create_date")
+    private LocalDateTime createDate;
+
+    @Column(name = "update_date")
+    private LocalDateTime updateDate;
+    
 	
 }
--- DroneInventorySystem/src/main/java/com/digitalojt/web/repository/CategoryInfoRepository.java
+++ DroneInventorySystem/src/main/java/com/digitalojt/web/repository/CategoryInfoRepository.java
@@ -1,10 +1,13 @@
 package com.digitalojt.web.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.digitalojt.web.entity.CategoryInfo;
 
 public interface CategoryInfoRepository extends JpaRepository<CategoryInfo, Integer> 
 {
-
+    // カテゴリ名でフィルタリングするメソッドを追加
+    List<CategoryInfo> findByCategoryName(String categoryName);
 }
